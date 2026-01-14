bc08f63e1055f557762eba8c6b0a706cd9dd9b06
Abhay Bhorkar <abhorkar@gmail.com>
2026-01-13 21:07:01 -0800

    Bump version to 8.1.7

--- financialadvisor/__init__.py
+++ financialadvisor/__init__.py
@@ -6,7 +6,7 @@ tax-advantaged projections, and detailed explanations.
 """
 
 # Version
-__version__ = "8.1.6"
+__version__ = "8.1.7"
 
 # Domain models
 from financialadvisor.domain.models import (
